8d9ee082eaec48b6ea2c0f1d0f69d35a82668c25
infololie-design <info.lolie@gmail.com>
2025-12-02 14:54:43 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // BURAYA KENDİ URL VE KEY BİLGİLERİNİ GİR
-const supabaseUrl = 'sb_publishable_tE_U041JjJ12n2lfoKG5WA_2C9ue8Dh'; 
+const supabaseUrl = 'https://eeesifwezjcsunjbmxsz.supabase.co'; 
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlZXNpZndlempjc3VuamJteHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MjM2OTMsImV4cCI6MjA3OTk5OTY5M30.vRiReyGh1I4EoAWetVPkaX_AuxBt_Xwxb_E30jPzp0s'; // Senin paylaştığın key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
