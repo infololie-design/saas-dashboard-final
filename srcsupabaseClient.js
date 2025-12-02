f099d0f19fdb78487fde2c56a56b4d475fb1ae70
infololie-design <info.lolie@gmail.com>
2025-12-02 14:33:38 +0300

    Delete srcsupabaseClient.js

--- srcsupabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// BURAYA KENDİ URL VE KEY BİLGİLERİNİ GİR
-const supabaseUrl = 'sb_publishable_tE_U041JjJ12n2lfoKG5WA_2C9ue8Dh'; 
-const supabaseAnonKey = 'sb_secret_3fdFRQQT6tDZ_xvN0iJOgg_38VvdA-U'; // Senin paylaştığın key
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
